4b92f63e6212f42a4033c24c2ef357856a2c38ae
Alexander <117478092+NanoBabaika@users.noreply.github.com>
2024-04-02 17:02:04 +0300

    Add files via upload

---
+++ scripts/slider__rewiev.js
@@ -0,0 +1,15 @@
+  $(document).ready(function(){
+    $('.bxslder').bxSlider({
+      slideWidth: 581,
+      minSlides: 3,
+      maxSlides: 3,
+      startSlide: 2,
+      slideMargin: 58,
+      pager:true,
+      controls:false
+      // ticker: true,
+      // speed: 22000,
+      // tickerHover:true,
+      
+    });
+  });
